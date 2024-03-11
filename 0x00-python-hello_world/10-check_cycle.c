3a34ebec9e1caa9457a32e4998ce96e06d016fe3
Caleb <Calebkyamaweb@gmail.com>
2024-03-12 01:06:20 +0300

    cycle-check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,22 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * check_cycle - function that checks if a singly linked list has a cycle.
- * @list: pointer to the of linked list.
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
  *
- * Only these functions are allowed: write, printf, putchar,
- * puts, malloc, free
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
 	listint_t *fast = list;
 
+	if (!list)
+		return (0);
 	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
-		if (fast == slow)
+		if (slow == fast)
 			return (1);
 	}
 	return (0);
