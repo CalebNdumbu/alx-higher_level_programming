de23422e17486ba2b051f6798bc24e6cde1b54f9
CalebNdumbu <calebkyamaweb@gmail.com>
2024-04-03 00:50:42 +0300

    Classes

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-        except Exception:
+    result = 0  # LOAD_CONST, STORE_FAST
+
+    for i in range(1, 3):  # SETUP_LOOP, GET_ITER, FOR_ITER
+        try:  # SETUP_EXCEPT
+            if i > a:  # COMPARE_OP, POP_JUMP_IF_FALSE
+                raise Exception('Too far')  # LOAD_GLOBAL, LOAD_CONST, CALL_FUNCTION, RAISE_VARARGS
+        except Exception:  # POP_TOP, POP_TOP, POP_TOP
             pass
-        else:
+        else:  # LOAD_FAST, BINARY_POWER, LOAD_FAST, BINARY_TRUE_DIVIDE, LOAD_FAST, BINARY_ADD, STORE_FAST
             result = (a ** b) / i + result
-        finally:
+        finally:  # POP_BLOCK, JUMP_ABSOLUTE, LOAD_FAST, BINARY_ADD, STORE_FAST, BREAK_LOOP
             result += a + b
             break
-    return result
+
+    return result  # LOAD_FAST, RETURN_VALUE
---
+++ 0x06. Python - Classes and Objects/1-square.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3
+"""Defines a class Square"""
+
+class Square:
+    """Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size):
+        """Initialize square class
+        """
+        self.__size = size
