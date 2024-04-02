e0cd5d0e569471b8e1cc6b668c041d73d3f46f21
CalebNdumbu <calebkyamaweb@gmail.com>
2024-04-02 13:24:50 +0300

    ident the return statement inside finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,18 +1,12 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
     """
-    Divides two integers and prints the result.
-
-    Args:
-        a: Integer numerator.
-        b: Integer denominator.
-
-    Returns:
-        The value of the division. If division by zero occurs, returns None.
+    Divides 2 integers and prints the result.
     """
     try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(result))
-        return result
+        print("Inside result: {}".format(div))
+        return (div)
