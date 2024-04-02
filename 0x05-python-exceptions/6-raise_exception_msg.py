2a665a5d017a33792aef9355979d091538d2378b
CalebNdumbu <calebkyamaweb@gmail.com>
2024-04-02 13:35:55 +0300

    6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -2,7 +2,8 @@
 
 def raise_exception_msg(message=""):
     """
-    function that raises a name exception with a message
+    function that raises a name exception with a message.
+    
     """
 
     raise NameError(message)
