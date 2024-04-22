d9054c1635dfc6a147feee7a5557f2bc98a1765f
CalebNdumbu <calebkyamaweb@gmail.com>
2024-04-22 18:13:35 +0300

    square args

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,12 +8,56 @@ class Square(Rectangle):
     "Represents a square"
 
     def __init__(self, size, x=0, y=0, id=None):
-       self.size = size
-       self.x = x
-       self.y = y
-       self.id = None
        super().__init__(size, size, x, y, id)
     
+    
+    @property
+    def size(self):
+        """Get the value of size"""
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """Setter method for size"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+
+            if len(args) >= 2:
+                self.size = args[1]
+
+            if len(args) >= 3:
+                self.x = args[2]
+
+            if len(args) >= 4:
+                self.y =args[3]
+
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+
+                elif key == 'size':
+                    self.size = value
+                
+                elif key == 'x':
+                    self.x = value
+
+                elif key == 'y':
+                    self.y = value
+
     def __str__(self):
         """Returns a string representation of the square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
