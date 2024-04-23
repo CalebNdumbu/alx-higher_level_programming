48ce147704639a67b9ca615d85d4fda9ac84b469
CalebNdumbu <calebkyamaweb@gmail.com>
2024-04-23 14:50:50 +0300

    final test

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -727,15 +727,6 @@ class TestSquare(unittest.TestCase):
         self.assertTrue(issubclass(Square, Base))
         self.assertFalse(isinstance(Square, Base))
 
-    # def test_10_3(self):
-    #     """Test Square class: check for missing args."""
-
-    #     with self.assertRaises(TypeError) as x:
-    #         s1 = Square()
-    #     self.assertEqual(
-    #         "__init__() missing 1 required positional argument: 'size'", str(
-    #             x.exception))
-
     def test_10_4(self):
         """Test Square for methods inherited from Rectangle."""
 
@@ -827,16 +818,6 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(len(s1_dictionary), len(s2_dictionary))
         self.assertEqual(type(s2_dictionary), dict)
         self.assertFalse(s1 == s2)
-
-    # def test_14_1(self):
-    #     """Test for public method to_dictionary with wrong args."""
-
-    #     s = "to_dictionary() takes 1 positional argument but 2 were given"
-    #     with self.assertRaises(TypeError) as x:
-    #         s1 = Square(10, 2, 1, 9)
-    #         s1_dictionary = s1.to_dictionary("Hi")
-    #     self.assertEqual(s, str(x.exception))
-
 
 if __name__ == '__main__':
     unittest.main()
